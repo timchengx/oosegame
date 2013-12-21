e75f4764e6b4b57082489bc31a3de23029663c52
Tim Cheng <tim@grommit.com>
2013-12-21 13:22:35 +0800

    remove useless file

--- src/com/oose/chinesechess/ChineseChessGameState.java
+++ src/com/oose/chinesechess/ChineseChessGameState.java
@@ -8,6 +8,7 @@ public class ChineseChessGameState extends GameState {
 
 	private static final long serialVersionUID = 4937630102339990622L;
 
+	/* 切不乾淨... (fallback) */
 	public ChineseChessGameState(String pOne, String pTwo, Bitmap pOnePic,
 			Bitmap pTwoPic, int fallback, int timeLimit) {
 		super(pOne, pTwo, pOnePic, pTwoPic, fallback, timeLimit);
--- src/com/oose/chinesechess/ChineseChessMan.java
+++ src/com/oose/chinesechess/ChineseChessMan.java
@@ -4,7 +4,10 @@ import com.oose.prototype.ChessBoard;
 import com.oose.prototype.ChessMan;
 
 abstract public class ChineseChessMan extends ChessMan {
-
+	/* 
+	 * chinese chess man don't need any addition field or method
+	 * but dark chess might be used.
+	 */
 	private static final long serialVersionUID = 400101495365023888L;
 
 	public ChineseChessMan(int x, int y, int belong, ChessBoard board) {
